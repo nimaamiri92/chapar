5b62131f16be8dfe18dbbaadebc1c16f1d945f87
nimak <nimaamiri92@gmail.com>
2019-11-16 00:23:52 +0330

    update config file

--- src/config/chapar.php
+++ src/config/chapar.php
@@ -9,14 +9,14 @@ return [
         'active' => false,
         'namespace' => Kavenegar::class,
         'credentials' => [
-            'key' => '505062376E3375477871376B646F462F7A4E575455413D3D'
+            'key' => ''
         ]
     ],
     'ghasedak' => [
         'active' => false,
         'namespace' => Ghasedak::class,
         'credentials' => [
-            'key' => 'bc79a7e229c7ebbe3fe1161c2190e3c73b26d9c37f4670a08534e3248524dbc7'
+            'key' => ''
         ]
     ],
     'MeliPayak' => [
